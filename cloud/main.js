dfc8fcdcbad68a47c3db72ddaefd5bef1002bfec
cloud-hot <wei.a.yang@gmail.com>
2015-05-14 20:33:40 +0800

    add two cloud function and modify the AppId and Key

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,27 @@ require("cloud/app.js");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.afterSave('_Followee', function(request) {
+  user = request.user
+  user.increment('followeeCount');
+  user.save(null, {
+    success: function(user) {
+      console.log('Increment of followee for :' + user.username);
+    },
+    error: function(user, error) {
+      throw 'Got an error ' + error.code + ' : ' + error.message;
+    }
+});
+
+AV.Cloud.afterSave('_Follower', function(request) {
+  user = request.user
+  user.increment('followerCount');
+  user.save(null, {
+    success: function(user) {
+      console.log('Increment of follower for :' + user.username);
+    },
+    error: function(user, error) {
+      throw 'Got an error ' + error.code + ' : ' + error.message;
+    }
+});
